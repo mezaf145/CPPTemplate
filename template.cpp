30bc18f64e235d1990eec241538aa39b68580492
aanoguchi <54706068+aanoguchi@users.noreply.github.com>
2019-11-22 11:08:21 -0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,67 @@
+/**
+ * 
+ * TODO: Standard Header
+ * Name
+ * Date
+ * Program summary
+ **/
+
+#include <iostream>
+#include <cassert>
+#include <string>
+
+using namespace std;
+
+/**
+ * Data Definitions 
+ * TODO: Write definitions for any data here
+ * EG:
+ * enum class Space {Empty, X, O};
+ * struct Row {
+ *   Space Left, Center, Right;
+ * }
+ **/
+
+/**
+ * Function Declarations
+ * TODO: Inert your function declarations, purpose statements, I/O comments here.
+ **/
+
+/**
+ * Declarations for given functions. Don't touch.
+ **/
+void test();
+void run();
+
+
+/**
+ * Main Function
+ *  Don't touch this! use `run()` instead
+ **/
+int main() {
+  char t;
+  cout << "Enter [t] to test, or any other character to run." << endl;
+  cin >> t;
+  if(t == 't') {
+    test();
+  } else {
+    run();
+  }
+}
+
+/**
+ * Function Definitions
+ *  Call a test function for each of your functions
+ **/
+void test() {
+  // TODO: write your point of entry code here.
+}
+
+// Use this in place of a main function
+void run() {c
+  // TODO: call your test functions here.
+}
+
+/**
+ * TODO: Insert your function definitions and unit test function definitions here.
+ **/
